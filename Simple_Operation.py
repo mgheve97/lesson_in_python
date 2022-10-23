2058ba0af1e6af97daa6e634c5d984e29dcc10dd
giziko97 <mgheve4@gmail.com>
2022-10-24 02:38:46 +0800

    backup

---
+++ Simple_Operation.py
@@ -0,0 +1,12 @@
+num1 = int(input("Enter First Number: "))
+num2 = int(input("Enter Second Number: "))
+
+sum = num1 + num2
+diff = num1 - num2
+prd = num1*num2
+quo = float(num1/num2)
+
+print("The Sum of ",num1 ," and ",num2," is ", sum)
+print("The Difference of ",num1 ," and ",num2," is ", diff)
+print("The Product of ",num1 ," and ",num2," is ", prd)
+print("The Quotient of ",num1 ," and ",num2," is ", quo)
